902110051e1654e4c9f074da8cd68768bc809949
Roman Volkov <volkovr1994@gmail.com>
2023-02-13 21:32:01 +0200

    Just started to write a code for matrix trace.

---
+++ MatrixTrace/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MatrixTrace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the quantity of columns for matrix:");
+            int quantityOfColumns = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the quantity of rows for matrix:");
+            int quantityOfRows = Convert.ToInt32(Console.ReadLine());
+            FillTheMatrix(quantityOfColumns, quantityOfRows);
+        }
+
+        public static int FillTheMatrix(int rows, int columns)
+        {
+            return rows * columns;
+        }
+
+
+        //new class 
+    }
+}
